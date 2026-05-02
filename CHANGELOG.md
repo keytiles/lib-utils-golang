fec21a9a0a0666df40bbec034ffb6da324e3c386
AttilaW <attila.wind@keytiles.com>
2026-05-02 08:06:22 +0200

    relrease 1.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,13 @@ We will mark these with Git Tags
 
 # Changes in releases
 
+## release 1.1.0
+
+New features:
+
+- A new function `ValueFromPtr()` is added to simplify boilerplates. Pair of `Ptr()` function which - by the way - is deprecated from now. See method docs
+  for reason.
+
 ## release 1.0.0
 
 Initial release
--- pkg/kt_utils/simple_funcs.go
+++ pkg/kt_utils/simple_funcs.go
@@ -1,4 +1,16 @@
 package kt_utils
 
-// Transforms anything into a pointer - useful for assigning values to pointer fields os structs / pointer vars
+// Transforms anything into a pointer - useful for assigning values to pointer fields or structs / pointer vars.
+//
+// Deprecated: from Go 1.26 the `new(expr)` built in does exactly the same for expressions and where you already have the value in an existing variable
+// `var v T` you can easily use `&v` anyways. So we keep this method for backward compatibility only.
 func Ptr[T any](t T) *T { return &t }
+
+// Helps to dereference a pointer safe way. If pointer is Nil the zero value is returned of the original type. Otherwise the dereferenced value.
+func ValueFromPtr[T any](ptr *T) T {
+	if ptr == nil {
+		var zero T
+		return zero
+	}
+	return *ptr
+}
